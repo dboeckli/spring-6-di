96e44c46ba8523fe2aa07d6ef36aba2a44fa3ffb
Dominique Boeckli <dboeckli@gmail.com>
2025-06-25 14:34:32 +0200

    fixup

--- src/test/java/ch/springframeworkguru/spring6di/Spring6diApplicationTest.java
+++ src/test/java/ch/springframeworkguru/spring6di/Spring6diApplicationTest.java
@@ -10,7 +10,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest(useMainMethod = SpringBootTest.UseMainMethod.ALWAYS)
 @Slf4j
-class Spring6diApplicationTests {
+class Spring6diApplicationTest {
 
     @Autowired
     private ApplicationContext applicationContext;
