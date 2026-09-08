6787095b50a0a4a8e14b2cc99859fcb2318a83c8
Dominique Boeckli <dboeckli@gmail.com>
2026-09-08 20:46:14 +0200

    chore(runner): migrate run configs to PowerShell, add test-k8s helm test runner
    - convert deploy-k8s/uninstall-k8s/clear docker from Shell Script to PowerShellRunType
    - fix stale spring-6-reactive-di chart filter and namespace in scripts
    - add test-k8s run config + script (helm test with pod cleanup)

--- .run/scripts/uninstall-k8s.ps1
+++ .run/scripts/uninstall-k8s.ps1
@@ -1,6 +1,6 @@
 cd target/helm/repo
 
-$file = Get-ChildItem -Filter spring-6-reactive-di-v*.tgz | Select-Object -First 1
+$file = Get-ChildItem -Filter spring-6-di-chart-*.tgz | Select-Object -First 1
 $APPLICATION_NAME = Get-ChildItem -Directory | Where-Object { $_.LastWriteTime -ge $file.LastWriteTime } | Select-Object -ExpandProperty Name
 
-helm uninstall $APPLICATION_NAME --namespace spring-6-reactive-di
+helm uninstall $APPLICATION_NAME --namespace spring-6-di
